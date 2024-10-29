e295a96c5b7dceecdc0d8903c7e78243bc112afb
od-8 <od30338@gmail.com>
2024-10-29 04:48:03 +0000

    Edited HTML an JS

--- notes.js
+++ notes.js
@@ -1,1 +1,14 @@
+function addNote () {
+    
+    let formElement = document.getElementById('form');
 
+    let newHTML = `
+        <input type="text" placeholder="Add note" id="new-text required></input> 
+        <label id="newther-note></label> `
+
+    formElement.innerHTML = newHTML
+}
+
+function removeNote () {
+    console.log('removed')
+}
