5801797aa4d8b35109314a285b6d06c716bcf8c6
od-8 <od30338@gmail.com>
2024-10-30 17:34:16 +0000

    Finished basic version

--- notes.js
+++ notes.js
@@ -1,14 +1,39 @@
+let formElement = document.getElementById('form');
+const input = document.querySelector(".input");
+let message = document.getElementById("message");
+
 function addNote () {
     
-    let formElement = document.getElementById('form');
+    
+    let HTMLString = `
+        <button id="completed" class="option" onclick="completeNote()" type="button">/</button>
+        <input type="text" placeholder="Do laundry..." id="new-text" required>
+        <button id="remove" class="option" onclick="removeNote()" type="button">X</button>`
 
-    let newHTML = `
-        <input type="text" placeholder="Add note" id="new-text required></input> 
-        <label id="newther-note></label> `
 
-    formElement.innerHTML = newHTML
+    input.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+function completeNote () {
+    //completedBtn.style.color = "White"
+    let newInput = document.getElementById('new-text')
+    if (newInput.value == '') {
+        message.textContent = "Please add a note"
+        setTimeout ( () => {
+            message.textContent = ''
+        }, 1000)
+    } else {
+        let completedBtn = document.getElementById('completed')
+        completedBtn.style.backgroundColor = "rgb(37, 185, 37)"
+        completedBtn.textContent = "Completed!";
+    }
 }
 
 function removeNote () {
-    console.log('removed')
+    let newInput = document.getElementById('new-text');
+    let completedBtn = document.getElementById('completed')
+    let removeBtn = document.getElementById('remove')
+    newInput.remove()
+    completedBtn.remove()
+    removeBtn.remove()
 }
